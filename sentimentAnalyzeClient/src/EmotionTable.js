ea0063046115da2538cd0cb34f9c268c12858ebf
Mozler <mustafahalitozler@gmail.com>
2022-05-30 09:53:37 +0000

    part 1 changes

--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -3,6 +3,9 @@ import './bootstrap.min.css';
 
 class EmotionTable extends React.Component {
     render() {
+        const emotionsArr = Object.entries(this.props.emotions)
+        const sortedArr = emotionsArr.sort((arr1,arr2)=>(arr2[1]-arr1[1])
+        )
       //Returns the emotions as an HTML table
       return (  
         <div>
@@ -12,6 +15,14 @@ class EmotionTable extends React.Component {
               /*Write code to use the .map method that you worked on in the 
               Hands-on React lab to extract the emotions. If you are stuck,
               please click the instructions to see how to implement a map*/
+              sortedArr.map(function(mapentry) {
+                return (
+                    <tr>
+                    <td>{mapentry[0]}</td>
+                    <td>{mapentry[1]}</td>
+                    </tr>
+                )
+                })
             }
             </tbody>
           </table>
